5ca938b4b7faa53ff73473db21cee6fd5c49aa1a
plugg1N <107611880+plugg1N@users.noreply.github.com>
2023-11-09 11:15:11 +0300

    new vers. 0.4.0

--- gms/GMSModule.py
+++ gms/GMSModule.py
@@ -1,4 +1,4 @@
-"""New Vers. 0.3.0"""
+"""New Vers. 0.4.0"""
 
 """    === General Model Selection Module (GMSModule) ===    """
 
@@ -43,24 +43,14 @@ Made by:
 ___
 """
 
-
-
-## Import necessary libraries
-
-# Math
 import math
-# Scorings for 'classification'
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-# Scorings for 'regression'
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error, r2_score
-# Beautify
 from tabulate import tabulate
 from tqdm import tqdm
-# For Dataframe
 import pandas as pd
                             
 
-## Main class
 class GMSModule:
     def __init__(self, *, mode: str, include: list, data: list, metrics: list, pivot: str = None) -> None:
         """
@@ -86,7 +76,7 @@ class GMSModule:
         Returns:
             Nothing.
         """
-        # Initiate variables
+
         self.mode = mode
         self.metrics = metrics
         self.include = include
@@ -95,10 +85,8 @@ class GMSModule:
         
         
         # Eval. functions to consider
-        
-        # Private
         self.__evaluation_functions = {
-            # For any classification task
+            # For classification
             'accuracy': accuracy_score,
             'precision': lambda y_true, y_pred: precision_score(y_true, y_pred, average='weighted'),
             'recall': lambda y_true, y_pred: recall_score(y_true, y_pred, average='weighted'),
@@ -120,7 +108,6 @@ class GMSModule:
         self.model_results = None
         
         
-        ## Catch possible user errors
         valid_modes = ['classification', 'regression']
         valid_data_types = ['list', 'tuple']
         valid_pivot_vals = list(self.__evaluation_functions.keys()) + [None]
@@ -144,7 +131,6 @@ class GMSModule:
             
 
             
-    # Protected
     def _evaluate_models(self) -> (list, list):
         """
         The Backbone of this module. Evaluate each model on different subsets using metrics given by the user
@@ -155,40 +141,45 @@ class GMSModule:
         Returns:
             A 'list' of 'tuple' values. Each tuple contains (*model_name, *dict_of_each_metric_evaluation). For both subsets: train and test
         """
+        
+        # Init. dicts to inflate with model_names as keys and predictions as values
+        self.model_predictions_dict_test = dict()
+        self.model_predictions_dict_train = dict()
+
+        
+        
         # Check if evaluation was already done
         if self.model_results is not None:
             return self.model_results
 
         else:
-            # Prepare lists for inflation
             results_test = []
             results_train = []
 
-            # Loop through each model given by a user
             for model in tqdm(self.include, desc="Evaluating Each Model", unit=" eval."):
                 model = model.fit(self.X_train, self.y_train)
                 
-                # Get predictions for each subset
+                
                 y_pred_test = model.predict(self.X_test)
                 y_pred_train = model.predict(self.X_train)
+                
+                self.model_predictions_dict_test[str(model)] = y_pred_test
+                self.model_predictions_dict_train[str(model)] = y_pred_train
 
+                
                 # Keep keys as metric names and evals as values
                 scores_test = {}
                 scores_train = {}
                     
                 # Evaluate each model on each metric given
                 for metric in self.metrics:
-                    # Get evaluation functions
                     evaluation_function = self.__evaluation_functions.get(metric)
                     
-                    # Inflate dicts
                     scores_test[metric] = evaluation_function(self.y_test, y_pred_test)
                     scores_train[metric] = evaluation_function(self.y_train, y_pred_train)
                 
                 
-                        
 
-                # Append results
                 results_test.append((model, scores_test))
                 results_train.append((model, scores_train))
 
@@ -213,17 +204,15 @@ class GMSModule:
             - if print_info=True: Nothing. Prints model name and score based on pivot
             - else: Best model as object[0] / Predictions on test dataset[1]
         """
-        
         # Make scores global to output scores to user
         global model_scores_dict_test
         
         result_train, result_test = self._evaluate_models()
         
-        # Initialize an empty dictionary, prepare for inflation
+
         model_scores_dict_train = {}
         model_scores_dict_test = {}
 
-        ## Iterate through the results and calculate the sum of scores | TRAIN
         
         # If self.pivot == None -> calculate sum of all scores
         if self.pivot == None: 
@@ -254,10 +243,8 @@ class GMSModule:
         if print_info:
             print(f"Model with the highest score: {best_model} \nWith scores: {model_scores_dict_test[best_model]}")
         else:
-            # Return best model object and predictions for further usage
             best_model_preds = best_model.predict(self.X_test)
             return best_model, best_model_preds
-        
         
         
         
@@ -272,7 +259,6 @@ class GMSModule:
         Returns:
             'dict' that consists of "Keys: 1-len(self.include), Values: models"
         """
-        # Evaluate models and get scores
         _, model_results = self._evaluate_models()
 
         # Create a ranking based on the sum of values in the dictionaries
@@ -293,6 +279,8 @@ class GMSModule:
         return ranking
     
     
+    
+    
     def to_df(self, subset: str = "test") -> pd.DataFrame:
         """
         Get a Dataframe of subset scores
@@ -303,13 +291,16 @@ class GMSModule:
         Returns:
             'pd.Dataframe' with model name and scores provided by user
         """
-        # Call the evaluation method to get the results.
-        results_train, results_test = self._evaluate_models()
+        if subset not in ['train', 'test']:
+            raise ValueError(f"Provided subset: '{subset}' is not in possible subsets: ['train', 'test']")
         
-        # Create columns to fill in the data
+        
+        results_train, results_test = self._evaluate_models()
+    
         
         # Model Names would always be as a column 
         columns = ['Model Name']
+        
         
         # For each metric provided create a column with its name
         for i in self.metrics:
@@ -318,7 +309,6 @@ class GMSModule:
         data = []
         
         if subset == "test":
-            # for each model given
             for i in range(len(self.include)):
                 # take its score predictions and create a list of [model_name, *scores] 
                 temp_data = [str(self.include[i])] + list(results_test[i][1].values())
@@ -329,13 +319,45 @@ class GMSModule:
             for i in range(len(self.include)):
                 temp_data = [str(self.include[i])] + list(results_train[i][1].values())
                 data.append(temp_data)
-                
-        else:
-            # Check if subset is not "train" or "test"
-            raise ValueError(f"Provided subset: '{subset}' is not in possible subsets: ['train', 'test']")
+            
             
         
         return pd.DataFrame(data=data, columns=columns)
+    
+    
+    
+    
+    def get_predictions(self, model, subset: str = "test") -> list:
+        """
+        Get predictions of a wanted model from the list of models provided.
+        
+        Args:
+            - model: An object of a model to get predictions from.
+            - subset: A string of a name of subset to get predictions from.
+                (default = "test")
+                
+        Returns:
+            List of predictions made by a model provided.
+        """
+        # Receive predictions
+        self._evaluate_models()
+        
+        # Check if model given is not evaluated by Module
+        if str(model) not in list(map(str, self.include)):
+            raise ValueError(f"Model name provided: '{model}' is not in evaluated models list: {self.include} ")
+        
+
+        if subset not in ['test', 'train']:
+            raise ValueError(f"Subset provided: '{subset}' is not 'train' or 'test'")
+            
+
+        if subset == "train":
+            return list(self.model_predictions_dict_train[str(model)])
+        
+        elif subset == "test":
+            return list(self.model_predictions_dict_test[str(model)])
+        
+        
         
         
       
@@ -357,7 +379,6 @@ class GMSModule:
         ranking = self.create_ranking()
         
         
-        # Print descriptions using each function GMSModule provides
         print("==== DESCRIPTION ====\n")
         
         # Show inputed info
@@ -379,4 +400,5 @@ class GMSModule:
         print("# 3. Ranking of each model:\n")
         for rank, model in ranking.items():
             print(f"{rank}: {model}")
- 
+            
+        
